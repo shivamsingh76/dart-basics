c780d8f9e8e3abc14c84536c6c8564c43a0c76e0
Shivam Singh <shivam.singh0706@gmail.com>
2025-11-08 14:49:43 +0530

    data types basic only

---
+++ dataTypes.dart
@@ -0,0 +1,24 @@
+void main() {
+  // Data Types in Dart
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  // Strings
+  String firstName = "John";
+  print("String: $firstName");
+
+  // Intergers
+  int myNum = 40;
+  print("Integer: $myNum");
+
+  // Doubles
+  double myDouble = 40.5;
+  print("Double: $myDouble");
+
+  // Booleans
+  bool myBool = false;
+  print("Boolean: $myBool");
+
+  // Dynamic
+  dynamic fullName = "John Doe";
+  print("Dynamic : $fullName");
+}
